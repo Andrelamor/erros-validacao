1700ddc3c541e553a2e83d5434ce159d6191a42b
andrelamor <amorim.andrel@gmail.com>
2022-03-14 19:38:12 -0300

    corrige build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 # Build pages
 livemark build 000_livemark/index.md --target ./index.html --config 000_livemark/livemark.yaml && \
-livemark build erros-validacao/001_setup/README.md --target erros-validacao/001_setup/README.html --config 000_livemark/livemark.yaml && \
-livemark build erros-validacao/002_warm_up/001_terminal_101/README.md --target erros-validacao/002_warm_up/001_terminal_101/README.html --config 000_livemark/livemark.yaml && \
-livemark build erros-validacao/002_warm_up/002_version_control_101/README.md --target erros-validacao/002_warm_up/002_version_control_101/README.html --config 000_livemark/livemark.yaml && \
+livemark build erros-validacao/001_metadados/datapackage-sintaxe.md --target erros-validacao/001_metadados/datapackage-sintaxe.html --config 000_livemark/livemark.yaml && \
+livemark build erros-validacao/001_metadados/datapackage-propriedades.md --target erros-validacao/001_metadados/datapackage-propriedades.html --config 000_livemark/livemark.yaml && \
 livemark build erros-validacao/002_dados/001_divergencias-metadados/divergencias-metadados.md --target erros-validacao/001_divergencias-metadados/divergencias-metadados.html --config 000_livemark/livemark.yaml && \
 livemark build erros-validacao/002_dados/002_delimitadores/delimitadores.md --target erros-validacao/002_dados/002_delimitadores/delimitadores.html --config 000_livemark/livemark.yaml && \
 livemark build erros-validacao/002_dados/003_encoding/encoding.md --target erros-validacao/002_dados/003_encoding/encoding.html --config 000_livemark/livemark.yaml && \
